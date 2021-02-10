4597f9bd330ac8660653f7a08f7b08ee2f08599c
win7 <win7@BILGISAYAR>
2021-02-10 23:34:52 +0300

    join was added between brand and car specials

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,50 +10,42 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //SpecialCarTest();
             //CarTest();
+            JoinTest(); 
+        }
+
+        private static void JoinTest()
+        {
             CarManager carManager = new CarManager(new EfCarDal());
             foreach (var car in carManager.GetProductDetail())
             {
-                Console.WriteLine(car.CarId+"/"+car.BrandName);
+                Console.WriteLine(car.CarId + "/" + car.BrandName);
             }
         }
 
         private static void CarTest()
         {
+            for (int i = 1; i < 9; i++)
+            {
+                CarManager carManager = new CarManager(new EfCarDal());
+                BrandManager brandManager = new BrandManager(new EfBrandDal());
+                ColorManager colorManager = new ColorManager(new EfColorDal());
+
+
+                Console.WriteLine("BrandId:------ {0} ------", i);
+                foreach (var cars in carManager.GetAllByBrandId(i))
+                {
+                    Console.WriteLine(cars.CarId + " \\ " + brandManager.GetById(cars.BrandId).BrandName + " \\ " + colorManager.GetByColorId(cars.ColorId).ColorName + " \\ " + cars.ColorId + " \\ " + cars.ModelYear + " \\ " + cars.Description + "\n");
+                }
+
+            }
+        }
+        private static void SpecialCarTest()
+        {
             CarManager carManager = new CarManager(new EfCarDal());
             BrandManager brandManager = new BrandManager(new EfBrandDal());
             ColorManager colorManager = new ColorManager(new EfColorDal());
-
-            Console.WriteLine("BrandId:------ 1 ------");
-            foreach (var cars in carManager.GetAllByBrandId(1))
-            {
-                Console.WriteLine(cars.CarId + " --- " + brandManager.GetById(cars.BrandId).BrandName + " --- " + colorManager.GetByColorId(cars.ColorId).ColorName + " --- " + cars.ColorId + " --- " + cars.ModelYear + " --- " + cars.Description + "\n");
-            }
-            Console.WriteLine("BrandId:------ 2 ------");
-            foreach (var cars in carManager.GetAllByBrandId(2))
-            {
-                Console.WriteLine(cars.CarId + " --- " + brandManager.GetById(cars.BrandId).BrandName + " --- " + colorManager.GetByColorId(cars.ColorId).ColorName + " --- " + cars.ColorId + " --- " + cars.ModelYear + " --- " + cars.Description + "\n");
-            }
-            Console.WriteLine("BrandId:------ 4 ------");
-            foreach (var cars in carManager.GetAllByBrandId(4))
-            {
-                Console.WriteLine(cars.CarId + " --- " + brandManager.GetById(cars.BrandId).BrandName + " --- " + colorManager.GetByColorId(cars.ColorId).ColorName + " --- " + cars.ColorId + " --- " + cars.ModelYear + " --- " + cars.Description + "\n");
-            }
-            Console.WriteLine("BrandId:------ 5 ------");
-            foreach (var cars in carManager.GetAllByBrandId(5))
-            {
-                Console.WriteLine(cars.CarId + " --- " + brandManager.GetById(cars.BrandId).BrandName + " --- " + colorManager.GetByColorId(cars.ColorId).ColorName + " --- " + cars.ColorId + " --- " + cars.ModelYear + " --- " + cars.Description + "\n");
-            }
-            Console.WriteLine("BrandId:------ 6 ------");
-            foreach (var cars in carManager.GetAllByBrandId(6))
-            {
-                Console.WriteLine(cars.CarId + " --- " + brandManager.GetById(cars.BrandId).BrandName + " --- " + colorManager.GetByColorId(cars.ColorId).ColorName + " --- " + cars.ColorId + " --- " + cars.ModelYear + " --- " + cars.Description + "\n");
-            }
-            Console.WriteLine("BrandId:------ 7 ------");
-            foreach (var cars in carManager.GetAllByBrandId(7))
-            {
-                Console.WriteLine(cars.CarId + " --- " + brandManager.GetById(cars.BrandId).BrandName + " --- " + colorManager.GetByColorId(cars.ColorId).ColorName + " --- " + cars.ColorId + " --- " + cars.ModelYear + " --- " + cars.Description + "\n");
-            }
             brandManager.Add(new Brand { BrandName = "a" });
             carManager.Add(new Car { CarId = 3, BrandId = 4, ColorId = 2, DailyPrice = 650, ModelYear = 2015, Description = "Full vehice" });
             carManager.Price(new Car { DailyPrice = 0 });
