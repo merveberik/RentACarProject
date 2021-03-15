372d6ec0f28738324b6dfa55eb680034b4e7350a
merve <merve@BILGISAYAR>
2021-03-15 23:10:53 +0300

    rentals was change

--- Entities/Concrete/Rental.cs
+++ Entities/Concrete/Rental.cs
@@ -11,7 +11,7 @@ namespace Entities.Concrete
         public int RentId { get; set; }
         public int CarId { get; set; }
         public int CustomerId { get; set; }
-        public int RentDate { get; set; }
-        public int ReturnDate { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime ReturnDate { get; set; }
     }
 }
